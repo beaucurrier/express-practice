5648b7ea235ef16eb99aceef71bae38ce9b16943
beau currier <beaucurrier01@gmail.com>
2024-09-10 13:59:15 -0700

    added a gitignore and a package,json and made a few small changes for the database

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
-const app = express();
 let books = require('./books-data').books;
+const Book = require("./models/Books")
+const mongoose = require("mongoose")
+require("dotenv").config
 
+const app = express();
+mongoose.connect(`${process.env.MONGO_DB_API_KEY}`)
 
 // Set the view engine to pug
 app.set('views', path.join(__dirname, 'views'));
@@ -16,16 +20,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 // Define routes here
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
+    const books = await Book.find();
     res.render('books', { books });
 });
-app.post("/", function (req, res) {
-    books.push(req.body)
+app.post("/", async function (req, res) {
+    await Book.create(req.body);
     res.redirect("/");
 });
-app.delete("/:id", function (req, res) {
-    const filteredList = books.filter((book) => book.isbn !== req.params.id);
-    books = filteredList;
+app.delete("/:id", async function (req, res) {
+    await Book.deleteOne({isbn:req.params.id});
     res.redirect("/");
 });
 
@@ -33,15 +37,14 @@ app.get("/new", function (req, res) {
     res.render("new-book");
 });
 
-app.get("/:id", function (req, res) {
+app.get("/:id", async function (req, res) {
     const isbn = req.params.id;
-    const book = books.find((book) => book.isbn === isbn);
+    const book = await Book.find({isbn: isbn})
     res.render("book-details", {book})
 })
-app.put('/:id', function(req, res) {
+app.put('/:id', async function(req, res) {
     const isbn = req.params.id;
-    const bookIndex = books.findIndex((book) => book.isbn === isbn);
-    books[bookIndex] = req.body;
+    await Book.findOneAndUpdate({isbn:isbn}, req.body)
     res.redirect("/");
 })
 
